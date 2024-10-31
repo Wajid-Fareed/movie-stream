1b55404af8fc3f5f8e3c662d6f911f8f48731318
Wajid-Fareed <wajidfareed103@gmail.com>
2024-10-31 11:07:23 +0500

    movie page

--- src/components/re-usable/card.tsx
+++ src/components/re-usable/card.tsx
@@ -1,22 +1,23 @@
 'use client'
 import { generateSlug } from '@/data/data';
-import { ICardMovie } from '@/types/type'
+import { ICardTMDB } from '@/types/type'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import { FaPlay } from 'react-icons/fa'
 
 interface CardProps {
-  card: ICardMovie;
+  card: ICardTMDB;
 }
 const Card: React.FC<CardProps> = ({ card }) => {
   const router = useRouter();
+   const imageBaseUrl = 'https://image.tmdb.org/t/p/w500'
   return (
-      <div className='relative group' onClick={() => router.push(`/movie/${generateSlug(card.title)}`)}>
-        <Image src={card.posterImg.src} alt={card.title} width={card.posterImg.width} height={card.posterImg.height} className='rounded-sm w-full h-auto'/>
-        <span className='px-2 py-1 bg-primarycolor text-white rounded-sm absolute top-4 right-4 font-medium shadow-md'>{card.quality}</span>
+      <div className='relative group overflow-hidden' onClick={() => router.push(`/movie/${generateSlug(card.title)}?id=${card.id}`)}>
+        <Image src={`${imageBaseUrl}${card.poster_path}`} alt={card.title} width={200} height={300} className='rounded-sm w-full h-auto group-hover:transform group-hover:scale-110 transition-transform duration-300'/>
+        <span className='px-2 py-1 bg-cta text-white rounded-sm absolute top-4 right-4 font-medium shadow-md'>{card.vote_average.toFixed(1)}</span>
         <div className='card-title absolute bottom-0 w-full rounded-sm flex justify-center pb-2'>
-          <h3 className='text-white font-medium text-lg'>{card.title}</h3>
+          <h3 className='text-white font-medium text-sm text-center'>{card.title}</h3>
         </div>
         <div className='absolute top-0 w-full h-full flex justify-center items-center bg-[rgba(0,0,0,.5)] rounded-sm cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-500'>
           <span className='rounded-full flex justify-center items-center w-20 h-20 bg-cta'><FaPlay size={30} className='text-primarycolor ps-1' /></span>
--- src/components/suggestion/Suggestion.tsx
+++ src/components/suggestion/Suggestion.tsx
@@ -1,27 +1,46 @@
-
-import Container from '../layout/Container'
-import Link from 'next/link'
-import { ICardMovie } from '@/types/type'
+'use client'
+import { useEffect, useState } from 'react';
+import Container from '../layout/Container';
+import Link from 'next/link';
+import { ICardTMDB } from '@/types/type';
 import Card from '../re-usable/card';
 
-interface SuggestionProps {
-  cardData: ICardMovie[];
-}
-const Suggestion: React.FC<SuggestionProps> = ({ cardData }) => {
-  return (
-    <Container className='mt-10'>
-      <Link href='/' className='px-6 py-3 font-medium bg-cta hover:bg-ctaHover text-white rounded-sm shadow-md'>Suggestion</Link>
-      <div className='grid grid-cols-6 mt-10'>
 
-        {
-          cardData.map((item) => (
-            <Card card={item} key={item.id} />
-          ))
+const Suggestion: React.FC = () => {
+  const [suggestions, setSuggestions] = useState<ICardTMDB[]>([]);
+
+  useEffect(() => {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+      }
+    };
+
+    fetch(url, options)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
         }
+        return res.json();
+      })
+      .then((json) => {
+        setSuggestions(json.results || []);
+      })
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
 
+  return (
+    <Container className="mt-10">
+      <Link href="/" className="px-6 py-3 font-medium bg-cta hover:bg-ctaHover text-white rounded-sm shadow-md">
+        Suggestion
+      </Link>
+      <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 xl:grid-cols-8 gap-5 mt-10">
+        {suggestions.map((item) => <Card card={item} key={item.id} />)}
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default Suggestion
+export default Suggestion;
